6eee67799256ffc684c5a9df32b57e91ea0c0f53
Rui Baltazar <rui.p.baltazar@gmail.com>
2014-04-16 23:54:17 +0800

    Finished #1. Transformed storyboard into gem

--- lib/story_branch.rb
+++ lib/story_branch.rb
@@ -1,6 +1,3 @@
-#!/usr/bin/env ruby
-# -*- coding: utf-8 -*-
-
 # Name: story_branch (recommend: setting a git alias as "git story")
 #
 # Author: Jason Milkins <jason@opsmanager.com> & Gabe Hollombe <gabe@neo.com>
@@ -46,157 +43,148 @@
 # Backlog:
 # ...
 
-def friendly_require gem, name=nil
-  begin
-    require gem
-  rescue LoadError
-    name = gem if name.nil?
-    puts "The #{name} gem must be installed"
-    exit
-  end
-end
-
-def env_required var_name
-  if ENV[var_name].nil?
-    puts "#{var_name} must be set in your environment."
-    exit
-  end
-  ENV[var_name]
-end
-
-# Readline wrapper with injected history
-def readline prompt, history=[]
-  if history.length > 0
-    history.each {|i| Readline::HISTORY.push i}
-  end
-  begin
-    Readline.readline(prompt, false)
-  rescue Interrupt
-    exit
-  end
-end
-
-def dashed s
-  s.tr(' _', '-')
-end
-
-# Branch name validation
-def validate_branch_name name
-  unless valid_branch_name? name
-    puts "Error: #{name}\nis an invalid name."
-    return false
-  end
-  existing_name_score = is_existing_branch?(name)
-  unless existing_name_score == -1
-    puts <<-END.strip_heredoc
-      Name Collision Error:
-
-      #{name}
-
-      This is too similar to the name of an existing
-      branch, a more unique name is required
-    END
-  end
-end
-
-def valid_branch_name? name
-  # Valid names begin with a letter and are followed by alphanumeric
-  # with _ . - as allowed punctuation
-  valid = /[a-zA-Z][-._0-9a-zA-Z]*/
-  name.match valid
-end
-
-# Git operations
-def is_existing_branch? name
-  # we don't use the Git gem's is_local_branch? because we want to
-  # ignore the id suffix while still avoiding name collisions
-  git_branch_names.each do |n|
-    normalised_branch_name = dashed n.match(/(^.*)(-[1-9][0-9]+$)?/)[1]
-    levenshtein_distance = Levenshtein.distance normalised_branch_name, name
-    if levenshtein_distance < 2
-      return levenshtein_distance
-    end
-  end
-  return -1
-end
-
-def git_branch_names
-  g = Git.open "."
-  g.branches.map(&:name)
-end
-
-def git_current_branch
-  g = Git.open "."
-  g.current_branch
-end
-
-def git_create_branch name
-  g = Git.open "."
-  g.branch(name).create
-  g.branch(name).checkout
-end
-
-# Use Pivotal tracker API to get Stories
-def list_pivotal_stories api_key, project_id
-  PivotalTracker::Client.token = api_key
-  project = PivotalTracker::Project.find(project_id.to_i)
-  stories = project.stories.all({current_state: :started})
-  stories.each_with_index{|s,i| puts "[#{i+1}] ##{s.id} : #{s.name}"}
-  stories
-end
-
-def select_story stories
-  story_selection = nil
-  while story_selection == nil or story_selection == 0 or story_selection > stories.length + 1
-    puts "invalid selection" if story_selection != nil
-    story_selection = readline("Select a story: ", Range.new(1,stories.length).to_a.map(&:to_s)).to_i
-  end
-  story = stories[story_selection - 1]
-  puts "Selected : ##{story.id} : #{story.name}"
-  return story
-end
-
-def create_feature_branch story
-  current_branch = git_current_branch
-  dashed_story_name = (dashed story.name)[0..50].downcase
-  feature_branch_name = nil
-  puts "You are checked out at: #{current_branch}"
-  if current_branch == "master"
-    while feature_branch_name == nil or feature_branch_name == ""
-      puts "Provide a new branch name..." if [nil, ""].include? feature_branch_name
-      feature_branch_name = readline("Name of feature branch: ", [dashed_story_name])
-    end
-    feature_branch_name.chomp!
-    validate_branch_name feature_branch_name
-    feature_branch_name_with_story_id = "#{feature_branch_name}-#{story.id}"
-    puts "Creating: #{feature_branch_name_with_story_id}"
-    git_create_branch feature_branch_name_with_story_id
-  else
-    puts "Feature branches must be created from 'master'"
-  end
-end
-
-def pivotal_story_branch api_key, project_id
-  stories = list_pivotal_stories api_key, project_id
-  story = select_story stories
-  create_feature_branch story
-end
-
-# -> init
-friendly_require 'pivotal-tracker'
-friendly_require 'readline'
-friendly_require 'git'
-friendly_require 'levenshtein', 'levenshtein-ffi'
-
-api_key = env_required 'PIVOTAL_API_KEY'
-project_id = File.read(".pivotal-id") rescue env_required('PIVOTAL_PROJECT_ID')
-
-# -> start
-pivotal_story_branch api_key, project_id
-
+require 'pivotal-tracker'
+require 'readline'
+require 'git'
+require 'levenshtein'
+require 'levenshtein-ffi'
 
 class StoryBranch
-  def self.hi
-    puts "hi"
+
+  def initialize(project_id_file_path=nil)
+    #confirm requirements - PIVOTAL_API_KEY and PIVOTAL_PROJECT_ID
+    @api_key = env_required 'PIVOTAL_API_KEY'
+    @project_id = (not project_id_file_path.nil?) ? (File.read(project_id_file_path) rescue env_required('PIVOTAL_PROJECT_ID')) : env_required('PIVOTAL_PROJECT_ID')
+  end
+
+  def connect
+    pivotal_story_branch @api_key, @project_id
+  end
+  
+  private
+  def env_required var_name
+    if ENV[var_name].nil?
+      puts "#{var_name} must be set in your environment."
+      exit
+    end
+    ENV[var_name]
+  end
+  
+  # Readline wrapper with injected history
+  def readline prompt, history=[]
+    if history.length > 0
+      history.each {|i| Readline::HISTORY.push i}
+    end
+    begin
+      Readline.readline(prompt, false)
+    rescue Interrupt
+      exit
+    end
+  end
+  
+  def dashed s
+    s.tr(' _', '-')
+  end
+  
+  # Branch name validation
+  def validate_branch_name name
+    unless valid_branch_name? name
+      puts "Error: #{name}\nis an invalid name."
+      return false
+    end
+    existing_name_score = is_existing_branch?(name)
+    unless existing_name_score == -1
+      puts <<-END.strip_heredoc
+        Name Collision Error:
+  
+        #{name}
+  
+        This is too similar to the name of an existing
+        branch, a more unique name is required
+      END
+    end
+  end
+  
+  def valid_branch_name? name
+    # Valid names begin with a letter and are followed by alphanumeric
+    # with _ . - as allowed punctuation
+    valid = /[a-zA-Z][-._0-9a-zA-Z]*/
+    name.match valid
+  end
+  
+  # Git operations
+  def is_existing_branch? name
+    # we don't use the Git gem's is_local_branch? because we want to
+    # ignore the id suffix while still avoiding name collisions
+    git_branch_names.each do |n|
+      normalised_branch_name = dashed n.match(/(^.*)(-[1-9][0-9]+$)?/)[1]
+      levenshtein_distance = Levenshtein.distance normalised_branch_name, name
+      if levenshtein_distance < 2
+        return levenshtein_distance
+      end
+    end
+    return -1
+  end
+  
+  def git_branch_names
+    g = Git.open "."
+    g.branches.map(&:name)
+  end
+  
+  def git_current_branch
+    g = Git.open "."
+    g.current_branch
+  end
+  
+  def git_create_branch name
+    g = Git.open "."
+    g.branch(name).create
+    g.branch(name).checkout
+  end
+  
+  # Use Pivotal tracker API to get Stories
+  def list_pivotal_stories api_key, project_id
+    PivotalTracker::Client.token = api_key
+    project = PivotalTracker::Project.find(project_id.to_i)
+    stories = project.stories.all({current_state: :started})
+    stories.each_with_index{|s,i| puts "[#{i+1}] ##{s.id} : #{s.name}"}
+    stories
+  end
+  
+  def select_story stories
+    story_selection = nil
+    while story_selection == nil or story_selection == 0 or story_selection > stories.length + 1
+      puts "invalid selection" if story_selection != nil
+      story_selection = readline("Select a story: ", Range.new(1,stories.length).to_a.map(&:to_s)).to_i
+    end
+    story = stories[story_selection - 1]
+    puts "Selected : ##{story.id} : #{story.name}"
+    return story
+  end
+  
+  def create_feature_branch story
+    current_branch = git_current_branch
+    dashed_story_name = (dashed story.name)[0..50].downcase
+    feature_branch_name = nil
+    puts "You are checked out at: #{current_branch}"
+    if current_branch == "master"
+      while feature_branch_name == nil or feature_branch_name == ""
+        puts "Provide a new branch name..." if [nil, ""].include? feature_branch_name
+        feature_branch_name = readline("Name of feature branch: ", [dashed_story_name])
+      end
+      feature_branch_name.chomp!
+      validate_branch_name feature_branch_name
+      feature_branch_name_with_story_id = "#{feature_branch_name}-#{story.id}"
+      puts "Creating: #{feature_branch_name_with_story_id}"
+      git_create_branch feature_branch_name_with_story_id
+    else
+      puts "Feature branches must be created from 'master'"
+    end
+  end
+  
+  def pivotal_story_branch api_key, project_id
+    stories = list_pivotal_stories api_key, project_id
+    story = select_story stories
+    create_feature_branch story
   end
 end
