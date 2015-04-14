78a142c13a5f51e7ed7b4c7e9daf11c598a5edf8
jasonm23 <jasonm23@gmail.com>
2015-04-14 18:57:34 +0800

    Update to 0.2.7 remove deprecated cli bins

--- README.md
+++ README.md
@@ -2,32 +2,37 @@
 
 # Story Branch
 
-A small collection of tools for working with git branches for
-Pivotal Tracker stories. Story branch, start, finish
+A small collection of tools for working with git branches and Pivotal
+Tracker stories. `git story`, `git finish`, `git start` and `git
+unstart`.
 
 ### Commentary
 
-`git story`: Create a git branch with automatic reference to a
+`git story`: Creates a git branch with automatic reference to a
 Pivotal Tracker Story. It will get started stories from your active
 project.  You can enter text and press TAB to search for a story
 name, or TAB to show the full list. It will then suggest an editable
 branch name. When the branch is created the `story_id` will
-be appended to it. Enter a blank to exit.
+be appended to it.
+
+e.g. `my-story-name-1234567`
+
+`git finish`: Creates a git commit message for the staged changes.
+
+e.g: `[Finishes #1234567] My Story Title`
+
+You must stage all changes (or stash them) first. Note the commit will not
+be pushed.  Note: You'll be able to bail out before.
 
 `git start`: Start a story in Pivotal Tracker from the terminal.
 It'll get all unstarted stories in your current project.  You can
 enter text and press TAB to search for a story name, or TAB to show
-the full list. Enter blank to exit.
+the full list.
 
-`git unstart`: Unstart a started story in Pivotal Tracker from the terminal.
+`git unstart`: Unstart a story in Pivotal Tracker from the terminal.
 It'll get all started stories in your current project.  You can
 enter text and press TAB to search for a story name, or TAB to show
-the full list. Enter blank to exit.
-
-`git finish`: Create commit/message for the staged changes, e.g:
-`"[Finishes #1234567] My Story Title"` - optionally Finishes the story
-via pivotal tracker's api. You must stage all changes (or stash
-them) first.
+the full list.
 
 ### Installing
 
@@ -35,46 +40,33 @@ Install the gem:
 
     gem install story_branch
 
-**Settings:** You must have a `PIVOTAL_API_KEY` environment variable set
+#### Settings
+
+You must have a `PIVOTAL_API_KEY` environment variable set
 to your Pivotal api key, plus either a `.story_branch` file or
 `PIVOTAL_PROJECT_ID` environment variable set. Note, values in
 `.story_branch` will override environment variable settings.
 
-**.story_branch file**
+#### .story_branch file
 
 A YAML file with either/both of:
 
     api: YOUR.PIVOTAL.API.KEY.STRING
     project: YOUR.PROJECT.ID.NUMBER
 
-Can be saved to `~/` or `./`
+Can be saved to `~/` or `./` (ie. your project folder)
 
 ### Usage
 
 You run story_branch from the git/project root folder.
 
-`start`, `unstart` and `story`, are run interactively and will display a
-list of stories to work with.
+`git story`, `git start` and `git unstart` are run interactively and
+will display a list of stories to work with.
 
-`finish` will scan the current branch name for a story id (as its
+`git finish` will scan the current branch name for a story id (as its
 suffix) and if a valid, active story is found on pivotal tracker it
 will create a commit with a message to trigger pivotal's git
 integraton features.
-
-All text entry supports Readline keyboard shortcuts / bindings, (word fwd/back, undo etc.)
-
-### Command names
-
-It's possible to the commands in a few ways, we have deprecated the
-old commmand names, and now encourage only the use of the `git`
-style usage.
-
-git style  | deprecated    | deprecated
----------- | ------------- | -------------
-git story  | story_branch  | story-branch
-git start  | story_start   | story-start
-git unstart| story_unstart | story-unstart
-git finish | story_finish  | story-finish
 
 ## Contributing
 
--- lib/story_branch.rb
+++ lib/story_branch.rb
@@ -3,33 +3,43 @@
 # Authors: Jason Milkins <jason@opsmanager.com>
 #          Rui Baltazar <rui.p.baltazar@gmail.com>
 #          Dominic Wong <dominic.wong.617@gmail.com>
+#          Ranhiru Cooray <ranhiru@gmail.com>
 #          Gabe Hollombe <gabe@neo.com>
 #
-# Version: 0.2.5
+# Version: 0.2.7
 #
 # ## Description
-#
-# A small collection of tools for working with git branches for
-# Pivotal Tracker stories. Story branch, start, finish
+# A small collection of tools for working with git branches and Pivotal
+# Tracker stories. `git story`, `git finish`, `git start` and `git
+# unstart`.
 #
 # ### Commentary
 #
-# `git story`: Create a git branch with automatic reference to a
+# `git story`: Creates a git branch with automatic reference to a
 # Pivotal Tracker Story. It will get started stories from your active
 # project.  You can enter text and press TAB to search for a story
 # name, or TAB to show the full list. It will then suggest an editable
 # branch name. When the branch is created the `story_id` will
 # be appended to it.
 #
+# e.g. `my-story-name-1234567`
+#
+# `git finish`: Creates a git commit message for the staged changes.
+#
+# e.g: `[Finishes #1234567] My Story Title`
+#
+# You must stage all changes (or stash them) first. Note the commit will not
+# be pushed.  Note: You'll be able to bail out before.
+#
 # `git start`: Start a story in Pivotal Tracker from the terminal.
 # It'll get all unstarted stories in your current project.  You can
 # enter text and press TAB to search for a story name, or TAB to show
 # the full list.
-
-# `git finish`: Create commit/message for the staged changes, e.g:
-# "[Finishes #1234567] My Story Title" - optionally Finishes the story
-# via pivotal tracker's api. You must stage all changes (or stash
-# them) first.
+#
+# `git unstart`: Unstart a story in Pivotal Tracker from the terminal.
+# It'll get all started stories in your current project.  You can
+# enter text and press TAB to search for a story name, or TAB to show
+# the full list.
 #
 # ### Installing
 #
@@ -37,43 +47,33 @@
 #
 #     gem install story_branch
 #
-# **Settings:** You must have a `PIVOTAL_API_KEY` environment variable set
+# #### Settings
+#
+# You must have a `PIVOTAL_API_KEY` environment variable set
 # to your Pivotal api key, plus either a `.story_branch` file or
 # `PIVOTAL_PROJECT_ID` environment variable set. Note, values in
 # `.story_branch` will override environment variable settings.
 #
-# **.story_branch file**
+# #### .story_branch file
 #
 # A YAML file with either/both of:
 #
 #     api: YOUR.PIVOTAL.API.KEY.STRING
 #     project: YOUR.PROJECT.ID.NUMBER
 #
-# Can be saved to `~/` or `./`
+# Can be saved to `~/` or `./` (ie. your project folder)
 #
 # ### Usage
 #
 # You run story_branch from the git/project root folder.
 #
-# `start`, `branch`, are run interactively and will display a
-# list of stories to work with.
+# `git story`, `git start` and `git unstart` are run interactively and
+# will display a list of stories to work with.
 #
-# `finish` will scan the current branch name for a story id (as its
+# `git finish` will scan the current branch name for a story id (as its
 # suffix) and if a valid, active story is found on pivotal tracker it
 # will create a commit with a message to trigger pivotal's git
 # integraton features.
-#
-# ### Command names
-#
-# It's possible to the commands in a few ways, we have deprecated the
-# old commmand names, and now encourage only the use of the `git`
-# style usage.
-#
-#       git style  | deprecated
-#      ------------+--------------+--------------
-#       git story  | story_branch | story-branch
-#       git start  | story_start  | story-start
-#       git finish | story_finish | story-finish
 #
 # ## Contributing
 #
@@ -123,7 +123,7 @@ module StoryBranch
           puts "No stories started, exiting"
           exit
         end
-        story   = @p.select_story stories
+        story = @p.select_story stories
         if story
           @p.create_feature_branch story
         end
@@ -173,20 +173,20 @@ module StoryBranch
 
         unless @p.is_current_branch_a_story?
           puts "Your current branch: '#{GitUtils.current_branch}' is not linked to a Pivotal Tracker story."
-          return
+          return nil
         end
 
         if GitUtils.has_status? :untracked or GitUtils.has_status? :modified
           puts "There are unstaged changes"
           puts "Use git add to stage changes before running git finish"
           puts "Use git stash if you want to hide changes for this commit"
-          return
+          return nil
         end
 
         unless GitUtils.has_status? :added or GitUtils.has_status? :staged
           puts "There are no staged changes."
           puts "Nothing to do"
-          return
+          return nil
         end
 
         puts "Use standard finishing commit message: [y/N]?"
@@ -235,7 +235,7 @@ module StoryBranch
     end
 
     def self.normalised_branch_name s
-      StringUtils.simple_sanitize((StringUtils.dashed s).downcase).squeeze("-")
+      simple_sanitize((dashed s).downcase).squeeze("-")
     end
 
     def self.strip_newlines s
@@ -243,7 +243,7 @@ module StoryBranch
     end
 
     def self.undashed s
-      s.underscore.humanize
+      s.gsub(/-/, " ").capitalize
     end
 
   end
@@ -295,7 +295,7 @@ module StoryBranch
     end
 
     def self.current_branch_story_parts
-      matches = current_branch.match(/(.*)-(\d+$)/)
+      matches = current_story
       if matches.length == 3
         { title: matches[1], id: matches[2] }
       else
@@ -357,10 +357,10 @@ module StoryBranch
 
     def is_current_branch_a_story?
       GitUtils.current_story and
-      GitUtils.current_story.length == 3 and
-      filtered_stories_list(:started, true).
-        map(&:id).
-          include? GitUtils.current_story[2].to_i
+        GitUtils.current_story.length == 3 and
+        filtered_stories_list(:started, true)
+        .map(&:id)
+        .include? GitUtils.current_story[2].to_i
     end
 
     def story_from_current_branch
