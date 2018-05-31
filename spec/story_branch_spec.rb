7e3562719aa6b48179ad850117807b501f7fa573
Rui Baltazar <rui.p.baltazar@gmail.com>
2018-05-31 23:58:05 +0900

    WIP - removed stupid test

--- spec/story_branch_spec.rb
+++ spec/story_branch_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe StoryBranch do
   it "has a version number" do
     expect(StoryBranch::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
