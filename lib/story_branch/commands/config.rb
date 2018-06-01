d52decc9e2c78be5a42cf8ef18dda9988b8660f8
Rui Baltazar <rui.p.baltazar@gmail.com>
2018-06-01 09:23:42 +0900

    WIP - checking for existing conf

--- lib/story_branch/commands/config.rb
+++ lib/story_branch/commands/config.rb
@@ -9,26 +9,43 @@ module StoryBranch
     class Config < StoryBranch::Command
       def initialize(options)
         @options = options
-        @config_file_path = Dir.home
-        @config_file_name = '.story_branch'
+        @config = init_config
       end
 
-      def execute(output: $stdout, **)
-        output.puts 'Looking for existing config file'
-        if File.exist? "#{@config_file_path}/#{@config_file_name}.yml"
-          output.puts 'Config file exists. Override?'
-        else
-          config = ::TTY::Config.new
-          config.filename = @config_file_name
-          config.append_path @config_file_path
-          prompt = ::TTY::Prompt.new
-          project_name = prompt.ask "What should be this project's name?"
-          api_key = prompt.ask 'Please provide the api key:'
-          project_id = prompt.ask "Please provide this project's id:"
-          config.set(project_name, :api_key, value: api_key)
-          config.set(project_name, :project_id, value: project_id)
-          config.write
-        end
+      def execute(input: $stdin, output: $stdout)
+        return if config_exist?
+        prompt = ::TTY::Prompt.new
+        project_name = prompt.ask "What should be this project's name?"
+        api_key = prompt.ask 'Please provide the api key:'
+        project_id = prompt.ask "Please provide this project's id:"
+        config.set(project_name, :api_key, value: api_key)
+        config.set(project_name, :project_id, value: project_id)
+        config.write
+      end
+
+      private
+
+      def init_config
+        config_file_path = Dir.home
+        config_file_name = '.story_branch'
+        config = ::TTY::Config.new
+        config.filename = config_file_name
+        config.append_path config_file_path
+        config
+      end
+
+      def config_exist?
+        return unless @config.persisted?
+        puts config_exist_message
+        true
+      end
+
+      def config_exist_message
+        <<-MESSAGE
+          Configuration file already exists.
+          Trying to add a new project? Use story_branch add
+          Migrating from old version? Use story_branch migrate
+        MESSAGE
       end
     end
   end
