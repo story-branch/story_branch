93f85a470783083b104796fa89e8bbcc27b3a842
Rui Baltazar <rui.p.baltazar@gmail.com>
2018-06-02 16:05:57 +0900

    WIP - fixing spec for add

--- spec/unit/add_spec.rb
+++ spec/unit/add_spec.rb
@@ -1,13 +1,43 @@
+require 'spec_helper'
 require 'story_branch/commands/add'
 
 RSpec.describe StoryBranch::Commands::Add do
-  it "executes `add` command successfully" do
-    output = StringIO.new
-    options = {}
-    command = StoryBranch::Commands::Add.new(options)
+  let(:prompt) { TTY::TestPrompt.new }
 
-    command.execute(output: output)
+  before do
+    allow(::TTY::Prompt).to receive(:new).and_return(prompt)
+    prompt.output << nil
+    prompt.input << "amazingkey\r123456\rtest-project"
+    prompt.input.rewind
 
-    expect(output.string).to eq("OK\n")
+    FakeFS.with_fresh do
+      FileUtils.mkdir_p Dir.home
+      command = StoryBranch::Commands::Add.new({})
+      command.execute
+    end
+  end
+
+  describe 'when there is no config file' do
+    it 'creates a new config in home directory' do
+      home_dir_config = "#{Dir.home}/.story_branch.yml"
+      expect(File.exist?(home_dir_config)).to eq true
+    end
+
+    describe 'prompting the user' do
+      it 'prompts the user for the project name' do
+        question = "What should be this project's name\?"
+        expect(prompt.output.string).to match(question)
+      end
+
+      it 'prompts the user for the api key' do
+        question = 'Please provide the api key:'
+        expect(prompt.output.string).to match(question)
+      end
+
+      it 'prompts the user for the project id' do
+        question = "Please provide this project's id:"
+        expect(prompt.output.string).to match(question)
+      end
+    end
   end
 end
