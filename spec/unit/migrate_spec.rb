53d8c3cf3bea432a39fd8bfc0ea4e63cf15c5a18
Rui Baltazar <rui.p.baltazar@gmail.com>
2018-06-02 16:26:01 +0900

    WIP - added todo for migrate specs

--- spec/unit/migrate_spec.rb
+++ spec/unit/migrate_spec.rb
@@ -1,13 +1,8 @@
 require 'story_branch/commands/migrate'
 
 RSpec.describe StoryBranch::Commands::Migrate do
-  it "executes `migrate` command successfully" do
-    output = StringIO.new
-    options = {}
-    command = StoryBranch::Commands::Migrate.new(options)
-
-    command.execute(output: output)
-
-    expect(output.string).to eq("OK\n")
-  end
+  # TODO:
+  # - When the user has configuration in local folder and home folder
+  # - When the user has configuration in env vars
+  # - When the user has configuration in home folder only
 end
