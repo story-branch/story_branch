54e686701f01bb5546fdd0c6315edeef05be17c3
Rui Baltazar <rui.p.baltazar@gmail.com>
2018-06-02 17:43:40 +0900

    WIP - rubocopying the git utils

--- lib/story_branch/git_utils.rb
+++ lib/story_branch/git_utils.rb
@@ -6,30 +6,22 @@ class StoryBranch::GitUtils
     ::Git.open '.'
   end
 
-  def self.is_existing_branch? name
+  def self.is_existing_branch?(name)
     branch_names.each do |n|
-      if Levenshtein.distance(n, name) < 3
-        return true
-      end
+      return true if Levenshtein.distance(n, name) < 3
       existing_branch_name = n.match(/(.*)(-[1-9][0-9]+$)/)
-      if existing_branch_name
-        levenshtein_distance = Levenshtein.distance existing_branch_name[1], name
-        if levenshtein_distance < 3
-          return true
-        end
-      end
+      next unless existing_branch_name
+      levenshtein_distance = Levenshtein.distance existing_branch_name[1], name
+      return true if levenshtein_distance < 3
     end
-    return false
+    false
   end
 
-  def self.is_existing_story? id
+  def self.is_existing_story?(id)
     branch_names.each do |n|
       branch_id = n.match(/-[1-9][0-9]+$/)
-      if branch_id
-        if branch_id.to_s == "-#{id}"
-          return true
-        end
-      end
+      next unless branch_id
+      return true if branch_id.to_s == "-#{id}"
     end
     false
   end
@@ -48,22 +40,19 @@ class StoryBranch::GitUtils
 
   def self.current_branch_story_parts
     matches = current_story
-    if matches.length == 3
-      { title: matches[1], id: matches[2] }
-    else
-      nil
-    end
+    return unless matches.length == 3
+    { title: matches[1], id: matches[2] }
   end
 
-  def self.create_branch name
+  def self.create_branch(name)
     g.branch(name).create
     g.branch(name).checkout
   end
 
-  def self.status_collect status, regex
+  def self.status_collect(status, regex)
     status.select{|e|
       e.match(regex)
-    }.map{|e|
+    }.map{ |e|
       e.match(regex)[1]
     }
   end
@@ -74,7 +63,7 @@ class StoryBranch::GitUtils
     staged_rx    = /^M  (.*)/
     added_rx     = /^A  (.*)/
     status = g.lib.send(:command, 'status', '-s').lines
-    return nil if status.length == 0
+    return nil if status.empty?
     {
       modified:  status_collect(status, modified_rx),
       untracked: status_collect(status, untracked_rx),
@@ -83,13 +72,12 @@ class StoryBranch::GitUtils
     }
   end
 
-  def self.has_status? state
+  def self.has_status?(state)
     return false unless status
-    status[state].length > 0
+    !status[state].empty?
   end
 
-  def self.commit message
+  def self.commit(message)
     g.commit(message)
   end
-
 end
