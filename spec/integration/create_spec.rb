56980fe6eec03cf8e5c373fa7f7250711ecc16c9
Rui Baltazar <rui.p.baltazar@gmail.com>
2018-06-02 17:44:59 +0900

    WIP - starting to fix integration

--- spec/integration/create_spec.rb
+++ spec/integration/create_spec.rb
@@ -1,5 +1,5 @@
-RSpec.describe "`story_branch create` command", type: :cli do
-  it "executes `story_branch help create` command successfully" do
+RSpec.describe '`story_branch create` command', type: :cli do
+  it 'executes `story_branch help create` command successfully' do
     output = `story_branch help create`
     expected_output = <<-OUT
 Usage:
@@ -8,7 +8,7 @@ Usage:
 Options:
   -h, [--help], [--no-help]  # Display usage information
 
-Command description...
+Create branch from estimated stories in pivotal tracker
     OUT
 
     expect(output).to eq(expected_output)
