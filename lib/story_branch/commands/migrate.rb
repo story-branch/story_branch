c47340d8c92306fa06415666eea88dbb6f2db21d
Rui Baltazar <rui.p.baltazar@gmail.com>
2018-06-03 12:54:41 +0900

    WIP - running spec on migrate

--- lib/story_branch/commands/migrate.rb
+++ lib/story_branch/commands/migrate.rb
@@ -11,6 +11,7 @@ module StoryBranch
     class Migrate < StoryBranch::Command
       GLOBAL_CONFIG_FILE = "#{ENV['HOME']}/.story_branch".freeze
       LOCAL_CONFIG_FILE = '.story_branch'.freeze
+      OLD_CONFIG_FILES = [LOCAL_CONFIG_FILE, GLOBAL_CONFIG_FILE].freeze
 
       def initialize(options)
         @options = options
@@ -20,101 +21,116 @@ module StoryBranch
       end
 
       def execute(_input: $stdin, output: $stdout)
-        return unless old_config_exists?
-        @config = init_config(ENV['HOME'])
-        return if config_exist?
-        migrate_keys
-        create_local_config
-        clean_old_config_files
-        output.puts 'Migration complete'
+        if missing_old_config?
+          output.puts old_config_file_not_found
+          return
+        end
+        # return unless old_config_exists?
+        # @config = init_config(ENV['HOME'])
+        # return if config_exist?
+        # migrate_keys
+        # create_local_config
+        # clean_old_config_files
+        # output.puts 'Migration complete'
       end
 
       private
 
-      def clean_old_config_files
-        [GLOBAL_CONFIG_FILE, LOCAL_CONFIG_FILE].each do |file|
-          FileUtils.rm file
-        end
-      end
-
-      def migrate_keys
-        migrate_key('api', 'PIVOTAL_API_KEY', :api_key)
-        migrate_key('project_id', 'PIVOTAL_PROJECT_ID', :project_id)
-        @config.write
-      end
-
-      def create_local_config
-        local_config = init_config('.')
-        local_config.set(:project_name, project_name)
-        local_config.write
-      end
-
-      # TODO: Move this somewhere else as it is the same as config command
-      def config_exist?
-        return unless @config.persisted?
-        puts config_exist_message
+      def missing_old_config?
+        OLD_CONFIG_FILES.each { |file| return false if File.exist?(file) }
+        return false if env_set?
         true
       end
 
-      def config_exist_message
-        <<-MESSAGE
-          Configuration file already exists. Have you migrated already?
-          Trying to add a new project? Use story_branch add
-        MESSAGE
+      def env_set?
+        ENV['PIVOTAL_API_KEY'].length.positive? ||
+          ENV['PIVOTAL_PROJECT_ID'].length.positive?
       end
 
-      def migrate_key(old_key, env, new_key)
-        value = config_value(old_key, env)
-        if value.nil?
-          puts cant_migrate_missing_value
-          exit 1
-        end
-        @config.set(project_name, new_key, value: value)
-      end
+  #     def clean_old_config_files
+  #       [GLOBAL_CONFIG_FILE, LOCAL_CONFIG_FILE].each do |file|
+  #         FileUtils.rm file
+  #       end
+  #     end
 
-      def project_name
-        return @project_name if @project_name
-        prompt = ::TTY::Prompt.new
-        @project_name = prompt.ask "What should be this project's name?"
-        @project_name
-      end
+  #     def migrate_keys
+  #       migrate_key('api', 'PIVOTAL_API_KEY', :api_key)
+  #       migrate_key('project_id', 'PIVOTAL_PROJECT_ID', :project_id)
+  #       @config.write
+  #     end
 
-      # TODO: Probably makes sense to move this to a common tty config
-      # utils file kind of thing as it will be shared by some commands.
-      def init_config(path)
-        config_file_path = path
-        config_file_name = '.story_branch'
-        config = ::TTY::Config.new
-        config.filename = config_file_name
-        config.append_path config_file_path
-        config
-      end
+  #     def create_local_config
+  #       local_config = init_config('.')
+  #       local_config.set(:project_name, project_name)
+  #       local_config.write
+  #     end
 
-      def old_config_exists?
-        return true if File.exist? GLOBAL_CONFIG_FILE
-        puts old_config_file_not_found
-        false
-      end
+  #     # TODO: Move this somewhere else as it is the same as config command
+  #     def config_exist?
+  #       return unless @config.persisted?
+  #       puts config_exist_message
+  #       true
+  #     end
+
+  #     def config_exist_message
+  #       <<-MESSAGE
+  #         Configuration file already exists. Have you migrated already?
+  #         Trying to add a new project? Use story_branch add
+  #       MESSAGE
+  #     end
+
+  #     def migrate_key(old_key, env, new_key)
+  #       value = config_value(old_key, env)
+  #       if value.nil?
+  #         puts cant_migrate_missing_value
+  #         exit 1
+  #       end
+  #       @config.set(project_name, new_key, value: value)
+  #     end
+
+  #     def project_name
+  #       return @project_name if @project_name
+  #       prompt = ::TTY::Prompt.new
+  #       @project_name = prompt.ask "What should be this project's name?"
+  #       @project_name
+  #     end
+
+  #     # TODO: Probably makes sense to move this to a common tty config
+  #     # utils file kind of thing as it will be shared by some commands.
+  #     def init_config(path)
+  #       config_file_path = path
+  #       config_file_name = '.story_branch'
+  #       config = ::TTY::Config.new
+  #       config.filename = config_file_name
+  #       config.append_path config_file_path
+  #       config
+  #     end
+
+  #     def old_config_exists?
+  #       return true if File.exist? GLOBAL_CONFIG_FILE
+  #       puts old_config_file_not_found
+  #       false
+  #     end
 
       def old_config_file_not_found
         <<-MESSAGE
-          Old configuration file not found in #{GLOBAL_CONFIG_FILE}
-          Trying to start from scratch? Use story_branch config
+Old configuration not found.
+Trying to start from scratch? Use story_branch add
         MESSAGE
       end
 
-      def cant_migrate_missing_value
-        <<-MESSAGE
-          Old configuration file not found in #{GLOBAL_CONFIG_FILE}
-          Trying to start from scratch? Use story_branch config
-        MESSAGE
-      end
+  #     def cant_migrate_missing_value
+  #       <<-MESSAGE
+  #         Old configuration file not found in #{GLOBAL_CONFIG_FILE}
+  #         Trying to start from scratch? Use story_branch add
+  #       MESSAGE
+  #     end
 
-      def config_value(key, env)
-        @old_config ||= ::YAML.load_file GLOBAL_CONFIG_FILE
-        return @old_config[key] if @old_config[key]
-        ENV[env]
-      end
+  #     def config_value(key, env)
+  #       @old_config ||= ::YAML.load_file GLOBAL_CONFIG_FILE
+  #       return @old_config[key] if @old_config[key]
+  #       ENV[env]
+  #     end
     end
   end
 end
--- spec/unit/migrate_spec.rb
+++ spec/unit/migrate_spec.rb
@@ -1,8 +1,35 @@
+require 'spec_helper'
 require 'story_branch/commands/migrate'
 
 RSpec.describe StoryBranch::Commands::Migrate do
+  describe 'when no configuration is found in any of the possibilities' do
+    let(:config_directory) { FileUtils.mkdir_p Dir.home }
+    let(:command) { StoryBranch::Commands::Migrate.new({}) }
+    let(:output) { ::StringIO.new }
+
+    before do
+      ENV['PIVOTAL_API_KEY'] = ''
+      ENV['PIVOTAL_PROJECT_ID'] = ''
+      FakeFS.with_fresh do
+        config_directory
+        command.execute(output: output)
+      end
+    end
+
+    it 'should print an informative error message' do
+      expected_message = <<-MESSAGE
+Old configuration not found.
+Trying to start from scratch? Use story_branch add
+      MESSAGE
+
+      expect(output.string).to eq expected_message
+    end
+  end
   # TODO:
-  # - When the user has configuration in local folder and home folder
+  # - When no configuration is found
+  # - it should output a message telling them what to do - aka run add
+
   # - When the user has configuration in env vars
+  # - When the user has configuration in local folder and home folder
   # - When the user has configuration in home folder only
 end
