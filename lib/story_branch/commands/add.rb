81769d974595443c79b731b87a23a8471e72e1ee
Rui Baltazar <rui.p.baltazar@gmail.com>
2018-06-04 00:13:43 +0900

    WIP - fixed logic for adding a new project

--- lib/story_branch/commands/add.rb
+++ lib/story_branch/commands/add.rb
@@ -22,23 +22,20 @@ module StoryBranch
 
       def create_local_config
         local_config = init_config('.')
-        local_config.set(:project_name, value: project_name)
+        local_config.set(:project_id, value: project_id)
         local_config.write
       end
 
       def create_global_config
         api_key = prompt.ask 'Please provide the api key:'
-        project_id = prompt.ask "Please provide this project's id:"
-        @config.set(project_name, :api_key, value: api_key)
-        @config.set(project_name, :project_id, value: project_id)
+        @config.set(project_id, :api_key, value: api_key)
         @config.write(force: true)
       end
 
-      def project_name
-        return @project_name if @project_name
-        prompt = ::TTY::Prompt.new
-        @project_name = prompt.ask "What should be this project's name?"
-        @project_name
+      def project_id
+        return @project_id if @project_id
+        @project_id = prompt.ask "Please provide this project's id:"
+        @project_id
       end
 
       def init_config(path)
--- spec/unit/add_spec.rb
+++ spec/unit/add_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe StoryBranch::Commands::Add do
 
   before do
     allow(::TTY::Prompt).to receive(:new).and_return(prompt)
-    prompt.input << "amazingkey\r123456\rtest-project"
+    prompt.input << "amazingkey\r123456"
     prompt.input.rewind
 
     FakeFS.with_fresh do
@@ -18,11 +18,6 @@ RSpec.describe StoryBranch::Commands::Add do
   end
 
   describe 'prompting the user' do
-    it 'prompts the user for the project name' do
-      question = "What should be this project's name\?"
-      expect(prompt.output.string).to match(question)
-    end
-
     it 'prompts the user for the api key' do
       question = 'Please provide the api key:'
       expect(prompt.output.string).to match(question)
@@ -41,8 +36,7 @@ RSpec.describe StoryBranch::Commands::Add do
       config.filename = '.story_branch'
       expect(config.persisted?).to eq true
       config.read
-      expect(config.fetch('test-project', :api_key)).to eq 'amazingkey'
-      expect(config.fetch('test-project', :project_id)).to eq '123456'
+      expect(config.fetch('123456', :api_key)).to eq 'amazingkey'
     end
 
     it 'creates a new local config file' do
@@ -51,7 +45,7 @@ RSpec.describe StoryBranch::Commands::Add do
       config.filename = '.story_branch'
       expect(config.persisted?).to eq true
       config.read
-      expect(config.fetch(:project_name)).to eq 'test-project'
+      expect(config.fetch(:project_id)).to eq '123456'
     end
   end
 
@@ -61,8 +55,7 @@ RSpec.describe StoryBranch::Commands::Add do
       config = TTY::Config.new
       config.append_path(Dir.home)
       config.filename = '.story_branch'
-      config.set('another-project', :api_key, value: 'amazingkey0000')
-      config.set('another-project', :project_id, value: '1234560000')
+      config.set('1234560000', :api_key, value: 'amazingkey0000')
       config.write
     end
 
@@ -72,10 +65,8 @@ RSpec.describe StoryBranch::Commands::Add do
       config.filename = '.story_branch'
       expect(config.persisted?).to eq true
       config.read
-      expect(config.fetch('another-project', :api_key)).to eq 'amazingkey0000'
-      expect(config.fetch('another-project', :project_id)).to eq '1234560000'
-      expect(config.fetch('test-project', :api_key)).to eq 'amazingkey'
-      expect(config.fetch('test-project', :project_id)).to eq '123456'
+      expect(config.fetch('1234560000', :api_key)).to eq 'amazingkey0000'
+      expect(config.fetch('123456', :api_key)).to eq 'amazingkey'
     end
 
     it 'creates a new local config file' do
@@ -84,7 +75,7 @@ RSpec.describe StoryBranch::Commands::Add do
       config.filename = '.story_branch'
       expect(config.persisted?).to eq true
       config.read
-      expect(config.fetch(:project_name)).to eq 'test-project'
+      expect(config.fetch(:project_id)).to eq '123456'
     end
   end
 end
