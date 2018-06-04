aa7fde748d08a80f1a613734251cc4dc6d967224
Rui Baltazar <rui.p.baltazar@gmail.com>
2018-06-04 14:47:53 +0800

    [Finishes #27] Allow config finish tag

--- lib/story_branch/pivotal_utils.rb
+++ lib/story_branch/pivotal_utils.rb
@@ -1,6 +1,6 @@
 class StoryBranch::PivotalUtils
   API_URL = 'https://www.pivotaltracker.com/services/v5/'
-  attr_accessor :api_key, :project_id
+  attr_accessor :api_key, :project_id, :finish_tag
 
   def valid?
     !@api_key.nil? && !@project_id.nil?
