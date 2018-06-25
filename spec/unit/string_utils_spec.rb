a3f687c0f8f9db60b618bb00823624c9a9a6af99
Rui Baltazar <rui.p.baltazar@gmail.com>
2018-06-25 18:48:30 +0800

    WIP - initial spec coverage for string utils

--- spec/unit/string_utils_spec.rb
+++ spec/unit/string_utils_spec.rb
@@ -1,6 +1,12 @@
+# frozen_string_literal: true
 require 'spec_helper'
+require 'story_branch/string_utils'
 
 RSpec.describe StoryBranch::StringUtils do
   describe 'dashed' do
+    it 'converts non alphabet to dash' do
+      dashed = StoryBranch::StringUtils.dashed("H_e,l l::o.W;h+o&?'Are'")
+      expect(dashed).to eq 'H-e-l-l--o-W-h-o---Are-'
+    end
   end
 end
