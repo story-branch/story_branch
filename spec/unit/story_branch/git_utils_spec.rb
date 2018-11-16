bc281dd9ae8e0fa3bb5da84d24ba5cc2367d52c2
Rui Baltazar <rui.p.baltazar@gmail.com>
2018-11-16 09:52:20 +0800

    WIP -

--- spec/unit/story_branch/git_utils_spec.rb
+++ spec/unit/story_branch/git_utils_spec.rb
@@ -4,6 +4,8 @@ require 'spec_helper'
 require 'story_branch/git_utils'
 
 RSpec.describe StoryBranch::GitUtils do
+  let(:fake_g) { instance_double('Git') }
+
   describe 'g' do
     before do
       allow(::Git).to receive(:open)
@@ -12,6 +14,18 @@ RSpec.describe StoryBranch::GitUtils do
     it 'uses Git gem to open the local git directory' do
       StoryBranch::GitUtils.g
       expect(Git).to have_received(:open).with('.')
+    end
+  end
+
+  describe 'branch_for_story_exists?' do
+    describe 'existing branches include the passed id' do
+      it 'returns true' do
+      end
+    end
+
+    describe 'existing branches does not include the passed id' do
+      it 'returns false' do
+      end
     end
   end
 end
