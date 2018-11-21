c415eca0b3365f0bd42bc39f555f869b15d20993
Rui Baltazar <rui.p.baltazar@gmail.com>
2018-11-21 11:50:24 +0400

    WIP - covered git utils

--- spec/unit/story_branch/git_utils_spec.rb
+++ spec/unit/story_branch/git_utils_spec.rb
@@ -6,6 +6,7 @@ require 'story_branch/git_wrapper'
 
 RSpec.describe StoryBranch::GitUtils do
   let(:distances) { [3, 4, 3, 4] }
+  let(:branches) { %w[amazing-name-1 amazing-feature-2] }
 
   before do
     allow(StoryBranch::GitWrapper).to receive(:branch_names).and_return(branches)
@@ -13,8 +14,6 @@ RSpec.describe StoryBranch::GitUtils do
   end
 
   describe 'existing_branch?' do
-    let(:branches) { %w[amazing-name-1 amazing-feature-2] }
-
     it 'determnines levenshtein distance between branch name and branch list' do
       StoryBranch::GitUtils.existing_branch?('new-branch-name')
       expect(Levenshtein).to have_received(:distance)
@@ -44,8 +43,6 @@ RSpec.describe StoryBranch::GitUtils do
   end
 
   describe 'branch_for_story_exists?' do
-    let(:branches) { %w[amazing-name-1 amazing-feature-2] }
-
     describe 'existing branches include the passed id' do
       it 'fetches all branches with command execution' do
         StoryBranch::GitUtils.branch_for_story_exists?(1)
@@ -61,6 +58,20 @@ RSpec.describe StoryBranch::GitUtils do
       it 'returns false' do
         expect(StoryBranch::GitUtils.branch_for_story_exists?(3)).to eq false
       end
+    end
+  end
+
+  describe 'current_branch_story_parts' do
+    let(:branch) { 'amazing-feature-1' }
+
+    before do
+      allow(StoryBranch::GitWrapper).to receive(:current_branch).and_return(branch)
+    end
+
+    it 'returns story title and id' do
+      expect(StoryBranch::GitUtils.current_branch_story_parts).to eq(
+        title: 'amazing-feature', id: 1
+      )
     end
   end
 end
