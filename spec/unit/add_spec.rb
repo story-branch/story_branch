f856ce8a2cd326a491064216ce533bd53a8abb4c
Rui <rui.p.baltazar@gmail.com>
2019-02-08 11:40:48 +0800

    [Resolves #60] When setting up a new machine it might duplicate the project configuration
    # Issue Title
    
    - When setting up a new machine it might duplicate the project configuration
    
    # Main changes
    
    - Before adding a new project id check if it is already existing in the local configuration file
    
    # Remove from here below if there is nothing to be added to the changelog
    CHANGELOG
     - Fixes duplication of project ids in local story_branch file
    --- 8< ---

--- spec/unit/add_spec.rb
+++ spec/unit/add_spec.rb
@@ -78,13 +78,68 @@ RSpec.describe StoryBranch::Commands::Add do
       expect(config.fetch('123456', :api_key)).to eq 'amazingkey'
     end
 
-    it 'appends to the local config file' do
-      config = TTY::Config.new
-      config.append_path('.')
-      config.filename = '.story_branch'
-      expect(config.persisted?).to eq true
-      config.read
-      expect(config.fetch(:project_id)).to eq %w[1234560000 123456]
+    describe 'when the local config does not have the project id' do
+      it 'appends to the local config file' do
+        config = TTY::Config.new
+        config.append_path('.')
+        config.filename = '.story_branch'
+        expect(config.persisted?).to eq true
+        config.read
+        expect(config.fetch(:project_id)).to eq %w[1234560000 123456]
+      end
+    end
+
+    describe 'when the local config already has the project id' do
+      let(:config_directory) do
+        FileUtils.mkdir_p Dir.home
+        config = TTY::Config.new
+        config.append_path(Dir.home)
+        config.filename = '.story_branch'
+        config.set('1234560000', :api_key, value: 'amazingkey0000')
+        config.write
+
+        local_config = TTY::Config.new
+        local_config.append_path('.')
+        local_config.filename = '.story_branch'
+        local_config.set(:project_id, value: '1234560000')
+        local_config.append('123456', to: :project_id)
+        local_config.write
+      end
+
+      it 'does nothing' do
+        config = TTY::Config.new
+        config.append_path('.')
+        config.filename = '.story_branch'
+        expect(config.persisted?).to eq true
+        config.read
+        expect(config.fetch(:project_id)).to eq %w[1234560000 123456]
+      end
+    end
+
+    describe 'when the local config only has that project id' do
+      let(:config_directory) do
+        FileUtils.mkdir_p Dir.home
+        config = TTY::Config.new
+        config.append_path(Dir.home)
+        config.filename = '.story_branch'
+        config.set('123456', :api_key, value: 'amazingkey')
+        config.write
+
+        local_config = TTY::Config.new
+        local_config.append_path('.')
+        local_config.filename = '.story_branch'
+        local_config.set(:project_id, value: '123456')
+        local_config.write
+      end
+
+      it 'does nothing' do
+        config = TTY::Config.new
+        config.append_path('.')
+        config.filename = '.story_branch'
+        expect(config.persisted?).to eq true
+        config.read
+        expect(config.fetch(:project_id)).to eq '123456'
+      end
     end
   end
 end
