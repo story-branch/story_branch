46046072725ca84b35951344d49a9f2a8d86d81e
Rui Baltazar <rui.p.baltazar@gmail.com>
2020-01-27 13:31:29 +0800

    WIP - sort spec

--- spec/unit/open_issue_spec.rb
+++ spec/unit/open_issue_spec.rb
@@ -4,17 +4,20 @@ require 'story_branch/url_opener'
 require 'story_branch/commands/open_issue'
 
 RSpec.describe StoryBranch::Commands::OpenIssue do
+  let(:output) { StringIO.new }
+  let(:options) { {} }
+
   before do
     allow(StoryBranch::UrlOpener).to receive(:open_url).and_return true
+    command = StoryBranch::Commands::OpenIssue.new(options)
+    command.execute(output: output)
   end
 
   it 'executes `open_issue` command successfully' do
-    output = StringIO.new
-    options = {}
-    command = StoryBranch::Commands::OpenIssue.new(options)
-
-    command.execute(output: output)
-
     expect(output.string).to eq('true')
+  end
+
+  it 'opens url' do
+    expect(StoryBranch::UrlOpener).to have_received(:open_url)
   end
 end
