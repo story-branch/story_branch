d188e3489c02a2c3cafdd645c79e7989e13dd630
Rui Baltazar <rui.p.baltazar@gmail.com>
2020-01-27 14:09:37 +0800

    WIP - passing all specs again

--- spec/unit/story_branch/main_spec.rb
+++ spec/unit/story_branch/main_spec.rb
@@ -355,8 +355,6 @@ RSpec.describe StoryBranch::Main do
     end
 
     describe 'when the feature id does not match a feature in the tracker' do
-      let(:story_double) { nil }
-
       it 'does nothing' do
         # TODO: make method call return something
         res = sb.story_finish
@@ -364,103 +362,98 @@ RSpec.describe StoryBranch::Main do
       end
     end
 
-    describe 'when there are untracked files' do
+    context 'when working on a story' do
       let(:story_double) do
         instance_double(StoryBranch::Jira::Issue, id: 'SB-123', title: 'yay')
       end
 
-      before do
-        allow(StoryBranch::GitUtils).to receive(:status?).and_return true
-      end
-
-      it 'prints the message informing the user' do
-        sb.story_finish
-        message = <<~MESSAGE
-          There are unstaged changes
-          Use git add to stage changes before running git finish
-          Use git stash if you want to hide changes for this commit
-        MESSAGE
-        expect(prompt).to have_received(:say).once.with(message)
-      end
-    end
-
-    describe 'when there are no changes to commit' do
-      let(:story_double) do
-        instance_double(StoryBranch::Jira::Issue, id: 'SB-123', title: 'yay')
-      end
-
-      before do
-        allow(StoryBranch::GitUtils).to receive(:status?).and_return false
-      end
-
-      it 'prints the message informing the user' do
-        sb.story_finish
-        message = <<~MESSAGE
-          There are no staged changes.
-          Nothing to do.
-        MESSAGE
-        expect(prompt).to have_received(:say).once.with(message)
-      end
-    end
-
-    describe 'when there are staged changes to be commited' do
-      let(:story_double) do
-        instance_double(StoryBranch::Jira::Issue, id: 'SB-123', title: 'yay')
-      end
-      let(:answer_to_no) { false }
-      let(:commit_message) { '[Finishes #SB-123] yay' }
-
-      before do
-        allow(StoryBranch::GitUtils).to receive(:status?) do |arg|
-          !%i[untracked modified].include? arg
-        end
-        sb.story_finish
-      end
-
-      context 'commit message is based on the settings' do
-        context 'if the value is set to a word' do
-          let(:finish_tag) { 'Bananas' }
-          let(:commit_message) { '[Bananas #SB-123] yay' }
-
-          it 'prompts the user to commit with default message' do
-            expect(prompt).to have_received(:yes?).once
-            expect(prompt).to have_received(:yes?)
-              .with("Commit with standard message? #{commit_message}")
-          end
+      describe 'when there are untracked files' do
+        before do
+          allow(StoryBranch::GitUtils).to receive(:status?).and_return true
         end
 
-        context 'if the value is set to an empty string' do
-          let(:finish_tag) { '' }
-          let(:commit_message) { '[#SB-123] yay' }
-
-          it 'prompts the user to commit with default message' do
-            expect(prompt).to have_received(:yes?).once
-            expect(prompt).to have_received(:yes?)
-              .with("Commit with standard message? #{commit_message}")
-          end
+        it 'prints the message informing the user' do
+          sb.story_finish
+          message = <<~MESSAGE
+            There are unstaged changes
+            Use git add to stage changes before running git finish
+            Use git stash if you want to hide changes for this commit
+          MESSAGE
+          expect(prompt).to have_received(:say).once.with(message)
         end
       end
 
-      it 'prompts the user to commit with default message' do
-        expect(prompt).to have_received(:yes?).once
-        expect(prompt).to have_received(:yes?)
-          .with("Commit with standard message? #{commit_message}")
+      describe 'when there are no changes to commit' do
+        before do
+          allow(StoryBranch::GitUtils).to receive(:status?).and_return false
+        end
+
+        it 'prints the message informing the user' do
+          sb.story_finish
+          message = <<~MESSAGE
+            There are no staged changes.
+            Nothing to do.
+          MESSAGE
+          expect(prompt).to have_received(:say).once.with(message)
+        end
       end
 
-      describe 'when the user says no' do
+      describe 'when there are staged changes to be commited' do
         let(:answer_to_no) { false }
+        let(:commit_message) { '[Finishes #SB-123] yay' }
 
-        it 'aborts the commit' do
-          expect(prompt).to have_received(:say).with('Aborted')
+        before do
+          allow(StoryBranch::GitUtils).to receive(:status?) do |arg|
+            !%i[untracked modified].include? arg
+          end
+          sb.story_finish
         end
-      end
 
-      describe 'when the user says yes' do
-        let(:answer_to_no) { true }
+        context 'commit message is based on the settings' do
+          context 'if the value is set to a word' do
+            let(:finish_tag) { 'Bananas' }
+            let(:commit_message) { '[Bananas #SB-123] yay' }
 
-        it 'commits with the message' do
-          expect(StoryBranch::GitWrapper).to have_received(:commit)
-            .with(commit_message)
+            it 'prompts the user to commit with default message' do
+              expect(prompt).to have_received(:yes?).once.with(
+                "Commit with standard message? #{commit_message}"
+              )
+            end
+          end
+
+          context 'if the value is set to an empty string' do
+            let(:finish_tag) { '' }
+            let(:commit_message) { '[#SB-123] yay' }
+
+            it 'prompts the user to commit with default message' do
+              expect(prompt).to have_received(:yes?).once.with(
+                "Commit with standard message? #{commit_message}"
+              )
+            end
+          end
+        end
+
+        it 'prompts the user to commit with default message' do
+          expect(prompt).to have_received(:yes?).once.with(
+            "Commit with standard message? #{commit_message}"
+          )
+        end
+
+        describe 'when the user says no' do
+          let(:answer_to_no) { false }
+
+          it 'aborts the commit' do
+            expect(prompt).to have_received(:say).once.with('Aborted')
+          end
+        end
+
+        describe 'when the user says yes' do
+          let(:answer_to_no) { true }
+
+          it 'commits with the message' do
+            expect(StoryBranch::GitWrapper).to have_received(:commit)
+              .with(commit_message)
+          end
         end
       end
     end
