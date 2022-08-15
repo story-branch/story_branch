8625519c0af394ace89d5258b10cd6aeaf68ecef
Rui Baltazar <rui.p.baltazar@gmail.com>
2022-08-15 15:35:23 +0800

    adding initial test for team

---
+++ spec/unit/story_branch/linear_app/team_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'story_branch/graphql'
+require 'story_branch/linear_app/team'
+
+RSpec.describe StoryBranch::LinearApp::Team do
+  let(:client) { StoryBranch::Graphql::Client.new(api_url: 'https://api.linear.app/', api_key: '1231123') }
+  let(:team_key) { 'BAN' }
+  let(:stories) do
+    OpenStruct.new(data: {
+      'viewer' => {
+        'assignedIssues' => {
+          'nodes' => [
+            'title' => 'Hello',
+            'number' => '123123',
+            'url' => 'https://googl.e'
+          ]
+        }
+      }
+    })
+  end
+
+  before do
+    allow(client).to receive(:get).and_return(stories)
+  end
+
+  describe 'stories' do
+    it 'fetches the stories from graphql client' do
+      team = described_class.new(team_key, client)
+      stories = team.stories
+
+      expect(stories.length).to eq 1
+      expect(stories[0].is_a?(StoryBranch::LinearApp::Issue)).to eq true
+      expect(stories[0].to_s).to eq 'BAN-123123 - Hello'
+    end
+  end
+end
